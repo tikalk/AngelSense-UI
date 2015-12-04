047cfccfdea4ec459a121ec8b809802e33cfe710
orizens <farhioren@gmail.com>
2015-12-04 14:13:57 +0200

    updated renderer to draw a route

--- src/client/app/map/map.renderer.srv.js
+++ src/client/app/map/map.renderer.srv.js
@@ -25,7 +25,7 @@
 
         // copied from 
         // https://developers.google.com/maps/documentation/javascript/examples/directions-waypoints
-        function calculateAndDisplayRoute(/*directionsService, directionsDisplay*/) {
+        function calculateAndDisplayRoute(start, end /*directionsService, directionsDisplay*/) {
         	var waypts = [];
         	// var checkboxArray = document.getElementById('waypoints');
         	// for (var i = 0; i < checkboxArray.length; i++) {
@@ -37,31 +37,19 @@
         	// 	}
         	// }
 
-        	directionsService.route({
-        		origin: document.getElementById('start').value,
-        		destination: document.getElementById('end').value,
+        	map.directionsService.route({
+        		origin: start,
+        		destination: end,
         		waypoints: waypts,
         		optimizeWaypoints: true,
         		travelMode: google.maps.TravelMode.DRIVING
-        	}, function(response, status) {
+        	}, onRouteReady);
+
+        	function onRouteReady (response, status) {
         		if (status === google.maps.DirectionsStatus.OK) {
-        			directionsDisplay.setDirections(response);
-        			var route = response.routes[0];
-        			var summaryPanel = document.getElementById('directions-panel');
-        			summaryPanel.innerHTML = '';
-			      // For each route, display summary information.
-			      for (var i = 0; i < route.legs.length; i++) {
-			      	var routeSegment = i + 1;
-			      	summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-			      	'</b><br>';
-			      	summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-			      	summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-			      	summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-			      }
-			  } else {
-			  	window.alert('Directions request failed due to ' + status);
-			  }
-			});
+        			map.directionsDisplay.setDirections(response);
+			    }
+			}
 		}
     }
 })();
